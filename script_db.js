46fa94bc9859a4f4af4c514d786abb97ef85bfb5
MarcosROS4 <rosairmao10@gmail.com>
2026-06-17 22:58:29 -0300

    Refactor database functions and add urgency parameter

--- script_db.js
+++ script_db.js
@@ -1,8 +1,6 @@
 const DATABASE_URL = "postgresql://neondb_owner:npg_0iubSolUF1Xt@ep-billowing-darkness-atcpgg5e-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
-
 const host = new URL(DATABASE_URL).host;
 const neonHttpEndPoint = `https://${host}/sql`;
-
 async function executarQueryNeon(querySQL, parametros = []) {
     try {
         const resposta = await fetch(neonHttpEndPoint, {
@@ -16,64 +14,42 @@ async function executarQueryNeon(querySQL, parametros = []) {
                 params: parametros
             })
         });
-
         if (!resposta.ok) {
              const errorTexto = await resposta.text();
              throw new Error(`Erro HTTP: ${errorTexto}`);
          }
-
         const dados = await resposta.json();
         return dados.rows;
     } catch (erro) {
-        console.error("Falha ao comunicar com o banco de dados:", 'error');
+        console.error("Falha ao comunicar com o banco de dados:", erro);
         throw erro;
     }
 }
-
-// ----- C - CREATE ----------------
-
 export async function insertTarefa(titulo, descricao, urgencia) {
-    console.log("Cadastrando usuario no banco: ", { titulo, descricao });
+    console.log("Cadastrando usuario no banco: ", { titulo, descricao, urgencia });
     const query = 'INSERT INTO tarefas (titulo, descricao, urgencia) VALUES($1, $2, $3) RETURNING *'
-
     const params = [titulo, descricao, urgencia];
-
     const linhas = await executarQueryNeon(query, params);
     return linhas !== null;
 }
-
-// -------- R - READ / LEITURA -------
-
 export async function consultarDiretoComFetch() {
     console.log("Buscando todas as tarefas...");
-    
-    const query = 'SELECT * FROM tarefas';  
 
+    const query = 'SELECT * FROM tarefas';
     const linhas = await executarQueryNeon(query);
     return linhas || [];
 }
-
-
-export async function sqlAtualizarTarefa(titulo, descricao, id) {
+export async function sqlAtualizarTarefa(titulo, descricao, id, urgencia) {
     console.log("Altualizando usuario no Banco, id: ", id);
-
-    const query = 'UPDATE tarefas SET titulo = $1, descricao = $2 WHERE id = $3 RETURNING *';
-
-    const params = [titulo, descricao, id];
-
+    const query = 'UPDATE tarefas SET titulo = $1, descricao = $2, urgencia = $3 WHERE id = $4 RETURNING *';
+    const params = [titulo, descricao, urgencia, id];
     const linhas = await executarQueryNeon(query, params);
     return linhas !== null;
 }
-
-// --- D - DELETE -------------------------
-
 export async function sqlDeletarTarefa(id) {
     console.log("Deletando usuario no Banco... ", id);
-
     const query = 'DELETE FROM tarefas WHERE id = $1 RETURNING *';
-
     const params = [id];
-
     const linhas = await executarQueryNeon(query, params)
     return linhas !== null;
 }
